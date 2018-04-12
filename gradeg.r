555f9c84ce13e110e731b4a40eb7258356371bb5
lichun <706701795@qq.com>
2018-04-12 19:34:02 +0800

    删除冗余的文件

--- gradeg.r
+++
@@ -1,8 +0,0 @@
-
- dir <- "C:\\Temp\\"
- file <- paste(dir,m$system[1],"_grade_",".png",sep="")
- ppi <- 300
- png(filename=file, width=6*ppi, height=6*ppi, res=ppi )
- chart_grades(m)  
- dev.off()
- chart_grades(m)  
